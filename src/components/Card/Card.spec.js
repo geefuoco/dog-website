1726f96922ea94d62366949dfd5555f80265f83c
Gianluca Fuoco <gianluca_1227@hotmail.com>
2021-12-09 17:23:09 -0500

    added the card component with tests

---
+++ src/components/Card/Card.spec.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { shallow } from "enzyme";
+import Card from "./Card";
+
+describe("Card", () => {
+  let wrapper;
+
+  beforeEach(() => (wrapper = shallow(<Card />)));
+
+  it("should render a div with class name card", () => {
+    expect(wrapper.find(".card").length).toEqual(1);
+  });
+});
