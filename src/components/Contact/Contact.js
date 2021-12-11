e4d876b69a5a24adf942d2538b4fbce7618be9dd
Gianluca Fuoco <gianluca_1227@hotmail.com>
2021-12-11 17:35:10 -0500

    Finished the Contact component along with tests and styles

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,7 +1,18 @@
 import "./Contact.scss";
 
 const Contact = () => {
-  return <div className="contact-container"></div>;
+  return (
+    <div className="contact-container">
+      <div className="contact-wrapper">
+        <h1>Want to find out more ?</h1>
+
+        <p>
+          We would love to hear from you ! Send us an email at
+          thebulldoghouse@example.com for any questions you may have.
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
