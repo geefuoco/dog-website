064376687c71d8c7852a0d4f60324841eafea071
Gianluca Fuoco <gianluca_1227@hotmail.com>
2021-12-11 17:35:58 -0500

    Finished the Home componenta along with styles and tests

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,28 @@
 import "./Home.scss";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
-  return <div className="home-container">Hello World</div>;
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("available-puppies");
+  };
+  return (
+    <div className="home-container">
+      <div className="wrapper">
+        <h1>Find the right bully for you and your family</h1>
+        <button
+          type="button"
+          tabIndex="0"
+          aria-pressed="false"
+          className="view-puppies"
+          onClick={handleClick}
+        >
+          View Puppies
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/components/Home/Home.spec.js
+++ src/components/Home/Home.spec.js
@@ -2,6 +2,13 @@ import React from "react";
 import { shallow } from "enzyme";
 import Home from "./Home";
 
+const mockedUsedNavigate = jest.fn();
+
+jest.mock("react-router-dom", () => ({
+  ...jest.requireActual("react-router-dom"),
+  useNavigate: () => mockedUsedNavigate,
+}));
+
 describe("Home", () => {
   let wrapper;
 
@@ -9,5 +16,9 @@ describe("Home", () => {
 
   it("should render a container called home container", () => {
     expect(wrapper.find(".home-container").length).toEqual(1);
+  });
+
+  it("should render a div called wrapper", () => {
+    expect(wrapper.find(".wrapper").length).toEqual(1);
   });
 });
