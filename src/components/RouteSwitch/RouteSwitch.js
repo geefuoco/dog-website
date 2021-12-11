e9403506ea7b0d5a05f06e5c7d642a17f73f33b5
Gianluca Fuoco <gianluca_1227@hotmail.com>
2021-12-11 17:37:11 -0500

    Added the navbar element to top level of page on RouteSwitch component

--- src/components/RouteSwitch/RouteSwitch.js
+++ src/components/RouteSwitch/RouteSwitch.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "../Navbar/Navbar";
 import Home from "../Home/Home";
 import AboutUs from "../AboutUs/AboutUs";
 import AvailablePuppies from "../AvailablePuppies/AvailablePuppies";
@@ -6,14 +7,20 @@ import Contact from "../Contact/Contact";
 
 const RouteSwitch = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/about-us" element={<AboutUs />}></Route>
-        <Route path="/available-puppies" element={<AvailablePuppies />}></Route>
-        <Route path="/contact" element={<Contact />}></Route>
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/about-us" element={<AboutUs />}></Route>
+          <Route
+            path="/available-puppies"
+            element={<AvailablePuppies />}
+          ></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 };
 
