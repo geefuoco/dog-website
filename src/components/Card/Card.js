80a70be35e1d19ebd0b0fc620acd175893ea7d78
Gianluca Fuoco <gianluca_1227@hotmail.com>
2021-12-11 17:34:38 -0500

    Finished the Card component along with test and styles

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,7 +1,27 @@
 import "./Card.scss";
+import PropTypes from "prop-types";
 
-const Card = () => {
-  return <div className="card"></div>;
+const Card = ({ image, alt, text, name, gender }) => {
+  return (
+    <div className="card">
+      <div className="card-image">
+        <img alt={alt} src={image} />
+      </div>
+      <div className="card-body">
+        <h2>Hi, I'm {name}</h2>
+        <p>Sex: {gender}</p>
+        <p>{text}</p>
+      </div>
+    </div>
+  );
+};
+
+Card.propTypes = {
+  image: PropTypes.string.isRequired,
+  alt: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  gender: PropTypes.string.isRequired,
 };
 
 export default Card;
--- src/components/Card/Card.spec.js
+++ src/components/Card/Card.spec.js
@@ -5,7 +5,18 @@ import Card from "./Card";
 describe("Card", () => {
   let wrapper;
 
-  beforeEach(() => (wrapper = shallow(<Card />)));
+  beforeEach(
+    () =>
+      (wrapper = shallow(
+        <Card
+          alt="no image"
+          image="no image"
+          name="tyson"
+          text="test text"
+          gender="male"
+        />
+      ))
+  );
 
   it("should render a div with class name card", () => {
     expect(wrapper.find(".card").length).toEqual(1);
